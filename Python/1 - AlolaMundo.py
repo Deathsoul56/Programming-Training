c23e8b87740323fdfd25a944276caef9c1700ff8
DeAthSoUl56 <rafa_tooie@hotmail.com>
2024-01-21 23:53:01 -0300

    Incorporan python y ruby

---
+++ Python/1 - AlolaMundo.py
@@ -0,0 +1,6 @@
+print("Alola mundo Python")
+
+x = 2       # Int
+y = 2.434   # Float
+A = "Cadena"        # String
+b = True            # Boolean
---
+++ Ruby/1 - AlolaMundo.rb
@@ -0,0 +1,26 @@
+puts "Alola Mundo Ruby"
+
+x = 123                             # Fixnum
+y = 9876543210                      # Bignum
+pi = 3.14                           # Float
+texto = "Cadena"                    # String
+arreglo = [1, 't', 3, "a", 5]       # Array
+b = true                            # Booleanos
+n = nil                             # Nil
+range = 1..9                        # Rangos
+
+puts "Tipos de datos"
+print "Fixnum: " , x , "\n"
+print "Bignum: " , y , "\n"
+print "Float: " , pi , "\n"
+print "String: " , texto , "\n"
+print "Array: " , arreglo , "\n"
+print "Booleanos: " , b , "\n"
+print "Nil: " , n , "\n"
+print "Rangos: " , range , "\n"
+
+
+
+persona = { "nombre" => "Juan", "edad" => 25, "ciudad" => "Ejemplo" }       #Hash
+
+print "Hash: " , persona , "\n"
