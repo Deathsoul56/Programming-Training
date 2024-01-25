0acff1858f827b007b010f56328dfcce0abb6e19
DeAthSoUl56 <rafa_tooie@hotmail.com>
2024-01-24 22:42:27 -0300

    agrega Swift

---
+++ Swift/1 - Alola.swift
@@ -0,0 +1,17 @@
+import Swift
+import Foundation
+
+// Hola Mundo en Swift
+print("Hello, World!")
+
+// Tipos de datos
+var x: Int = 42                            // speed is an Int
+var pi: Float = 3.14                      // pi is a Float
+var y: Double = 1_000_000_000            // giga is a Double
+var z: Double = 6.62607015e-34          // plancksConstant is a Double
+var bol: Bool = true
+
+print(pi)
+print(y)
+print(z)
+print(bol)
