5383bb8a2395f449beda89b738b980319c09c6dc
DeAthSoUl56 <rafa_tooie@hotmail.com>
2024-06-10 23:43:18 -0400

    Agregan distribucion binomial y lognormal

--- Ruby/5 - Random.rb
+++ Ruby/5 - Random.rb
@@ -2,26 +2,26 @@ require 'securerandom'
 require 'rubystats'
 
 # Números Pseudo-Aleatorios
-aleatorio = rand # Numero aleatorio entre 0 y 1
+aleatorio = rand # Número aleatorio entre 0 y 1
 puts "Número aleatorio entre 0 y 1: #{aleatorio}"
 
-aleatorio = rand(-10..10) # Numero aleatorio entre -10 y 10
-puts "Número aleatorio: #{aleatorio}"
+aleatorio = rand(-10..10) # Número aleatorio entre -10 y 10 [-10, 10]
+puts "Número aleatorio entre -10 y 10: #{aleatorio}"
 
-aleatorio = Array.new(10) { rand(-10..10) } # Vector 10 números aleatorios
-puts "Vector aletario: #{aleatorio}"
+aleatorio = Array.new(10) { rand(-10..10) } # Vector de 10 números aleatorios entre -10 y 10 [-10, 10]
+puts "Vector aleatorio: #{aleatorio}"
 
 aleatorio = Array.new(5) { rand } # Muestra de 5 valores aleatorios
-puts "Muestra aleatorio: #{aleatorio}"
+puts "Muestra aleatoria: #{aleatorio}"
 
 aleatorio = SecureRandom.random_bytes(12) # Bytes aleatorios
-puts "Bytes Aleatorios: #{aleatorio}"
+puts "Bytes aleatorios: #{aleatorio.inspect}" # inspect para mostrar bytes legiblemente
 
-aleatorio = Array.new(4) { Array.new(3) { rand(-10..10) } } # Matriz 4x3 aleatorios
-puts "Matriz aletario: #{aleatorio}"
+aleatorio = Array.new(4) { Array.new(3) { rand(-10..10) } } # Matriz 4x3 de aleatorios
+puts "Matriz aleatoria: #{aleatorio}"
 
-aleatorio = Array.new(2) { Array.new(2) { rand } } # Matriz 2x2 aleatorios entre 0 y 1
-puts "Matris aletario entre 0 y 1: #{aleatorio}"
+aleatorio = Array.new(2) { Array.new(2) { rand } } # Matriz 2x2 aleatoria entre 0 y 1
+puts "Matriz aleatoria entre 0 y 1: #{aleatorio}"
 
 vector = [7, -4, -3, 7, 9, 7, 0, -5, 3, 3]
 v = vector.sample
@@ -30,7 +30,7 @@ puts "El vector v = #{vector} se escoge al azar un valor #{v}"
 # Permutaciones
 lista = ["rojo", "verde", "azul", "amarillo", "rosa", "violeta", "blanco", "negro"]
 puts "Lista Original = #{lista}"
-lista.shuffle! # Funciones para revolver un array
+lista.shuffle! # Función para revolver un array
 puts "Lista revuelta = #{lista}"
 
 secuencia = (1..10).to_a
@@ -42,10 +42,29 @@ puts "Permutación aleatoria: #{permutacion}"
 mu = 5 # Media
 sd = 8 # Desviación estándar
 
-# Valor aleatorio con distribución normal (media, Sx)
-normal = Rubystats::NormalDistribution.new(mu, sd).rng
+normal = Rubystats::NormalDistribution.new(mu, sd).rng # Valor aleatorio con distribución normal (media, Sx)
 puts "Valor aleatorio con distribución normal media: #{mu} desviación estándar: #{sd} = #{normal}"
 
-# Vector aleatorio con distribución normal (media, Sx)
-normals = Rubystats::NormalDistribution.new(mu, sd).rng(10)
-puts "Vector aleatorio con distribución normal media: #{mu} desviación estándar: #{sd} = #{normals}"
+normal = Rubystats::NormalDistribution.new(mu, sd).rng(10) # Vector aleatorio con distribución normal (media, Sx)
+puts "Vector aleatorio con distribución normal media: #{mu} desviación estándar: #{sd} = #{normal}"
+
+# Distribución Log-Normal
+mu_x = 1 # Parámetro localización
+sd_x = 2 # Parámetro Forma
+
+lognormal_dist = Rubystats::LognormalDistribution.new(mu_x, sd_x) # Inicializa la distribucion
+lognormal = lognormal_dist.rng # Valor aleatorio con distribución Log-normal (mu_x, sd_x)
+puts "Valor aleatorio con distribución log-normal localización: #{mu_x} forma: #{sd_x} = #{lognormal}"
+
+lognormal = Array.new(10) { lognormal_dist.rng } # Vector aleatorio con distribución Log-normal (mu_x, sd_x)
+puts "Vector aleatorio con distribución log-normal localización: #{mu_x} forma: #{sd_x} = #{lognormal}"
+
+# Distribuciones de probabilidad discretas
+# Distribución Binomial
+N = 10 # Cantidad de intentos
+Prop = 0.25 # Probabilidad de éxito
+binomial = Rubystats::BinomialDistribution.new(N, Prop).rng # Valor aleatorio con distribución Binomial (N, Pi)
+puts "Valor aleatorio con distribución Binomial Intentos: #{N} Probabilidad: #{Prop} = #{binomial}"
+
+binomial = Rubystats::BinomialDistribution.new(N, Prop).rng(10) # Vector aleatorio con distribución binomial (N, Pi)
+puts "Vector aleatorio con distribución Binomial Intentos: #{N} Probabilidad: #{Prop} = #{binomial}"
