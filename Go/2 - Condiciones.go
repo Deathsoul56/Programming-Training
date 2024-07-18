bb03c8785a6de1dd1e1ea2ab44219abb717d0ba7
DeAtSoUl56 <rafa_tooie@hotmail.com>
2024-07-18 17:04:27 -0400

    Agrega ejemplo de CASE

--- Go/2 - Condiciones.go
+++ Go/2 - Condiciones.go
@@ -52,4 +52,27 @@ func main() {
 	} else {
 		fmt.Println("ninguna condición se cumple")
 	}
+
+	// Condicion tipo Case
+
+	day := 3
+
+	switch day {
+	case 1:
+		fmt.Println("Lunes")
+	case 2:
+		fmt.Println("Martes")
+	case 3:
+		fmt.Println("Miércoles")
+	case 4:
+		fmt.Println("Jueves")
+	case 5:
+		fmt.Println("Viernes")
+	case 6:
+		fmt.Println("Sábado")
+	case 7:
+		fmt.Println("Domingo")
+	default:
+		fmt.Println("Día inválido")
+	}
 }
