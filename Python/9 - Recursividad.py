eaab3510eaf03e0f1b72782969e239f2fe93b862
DeAthSoUl56 <rafa_tooie@hotmail.com>
2025-02-01 23:02:07 -0300

    [MEJORAS] ejercicio 9 python: Se agrega validadcion de la variable

--- Python/9 - Recursividad.py
+++ Python/9 - Recursividad.py
@@ -1,10 +1,14 @@
 def factorial(n):
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("El factorial solo está definido para enteros no negativos.") # raise es para lanzar excepciones de manera manual
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
 
 def fibonacci(n):
+    if n < 1:
+        raise ValueError("El índice debe ser un entero positivo.")
     if n == 1 or n == 2:
         return 1
     else:
